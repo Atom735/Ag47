4e47173f245acb67bfb0218e292a39c4c4aac72e
Atom735 <Atom735.ag47@gmail.com>
2020-06-20 17:29:45 +0300

    add functions for get encoding of files

--- bin/mapping.dart
+++ bin/mapping.dart
@@ -2,7 +2,11 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:math';
 
-/// Поиск
+import 'dart:typed_data';
+
+import 'package:dart_test/freq_2letters.dart';
+
+/// Поиск кодировок
 Future<Map<String, List<String>>> loadMappings(final String path) async {
   final map = <String, List<String>>{};
   await for (final e in Directory(path).list(recursive: false)) {
@@ -31,4 +35,39 @@ Future<Map<String, List<String>>> loadMappings(final String path) async {
     }
   }
   return map;
+}
+
+/// Возвращает актуальность той или иной кодировки
+Map<String, int> getMappingRaitings(
+    final Map<String, List<String>> map, final Uint8List bytes) {
+  final r = <String, int>{};
+  map.forEach((k, v) {
+    r[k] = 0;
+  });
+  var byteLast = 0;
+  for (final byte in bytes) {
+    if (byte >= 0x80 && byteLast >= 0x80) {
+      // map.forEach(k,v) {
+      //   // r[i] += freq_2letters(map[i][byteLast - 0x80] + map[i][byte - 0x80]);
+      // }
+      map.forEach((final k, final v) {
+        r[k] += freq_2letters(v[byteLast - 0x80] + v[byte - 0x80]);
+      });
+    }
+    byteLast = byte;
+  }
+  return r;
+}
+
+/// Возвращает актуальную кодировку
+String getMappingMax(final Map<String, int> r) {
+  var o = '';
+  r.forEach((final k, final v) {
+    if (r[o] == null) {
+      o = k;
+    } else if (r[o] < v) {
+      o = k;
+    }
+  });
+  return o;
 }
