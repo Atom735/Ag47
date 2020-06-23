d6a652fad9943bac070685479837b32e8b1cd52b
Atom735 <Atom735.ag47@gmail.com>
2020-06-23 15:02:12 +0300

    restructed isolates

--- bin/main.dart
+++ bin/main.dart
@@ -1,178 +1,185 @@
 import 'dart:async';
+import 'dart:collection';
 import 'dart:convert';
 import 'dart:io';
 import 'dart:isolate';
 
 import 'mapping.dart';
 
+/// Данные лога
+class LasMethodData {
+  String mnem;
+  String unit;
+  String apiCode;
+  String description;
+  String strt;
+  String stop;
+  String step;
+}
+
+/// Разобранные данные
+class LasFile {
+  // ~V
+  int vVersion;
+  bool vWrap;
+  // ~W
+  String wStrt;
+  String wStop;
+  String wStep;
+  String wNull;
+  String wWell;
+  String wDate;
+  // ~C
+  List<LasMethodData> methods;
+}
+
+/**
+ * Главный изолят:
+ * * Обходит всё дерево и все файлы
+ * * Вскрывает архивы с помощью 7zip и обходит их
+ * * Передаёт пути к файлам вспомогательным изолятам для разбора
+ * * Создаёт конечную таблицу
+ *
+ * Вспомогательный изолят:
+ * * Обрабатывает файлы (сам пытается понять что за файл)
+ * * Las: файлы с методами ГИС
+ * * * Подбирает кодировку
+ * * * Разбирает и возвращает данные
+ * * Inc(txt): файлы инклинометрии (текстовый)
+ * * * Подбирает кодировку
+ * * * Разбирает и возвращает данные
+ * * Inc(doc): файлы инклинометрии (бинарный Word)
+ * * * Транслирует в docx и обрабатывает его
+ * * * Удаляет временный docx файл
+ * * Inc(docx): файлы инклинометрии (современный Word)
+ * * * Разархивировать его во временную папку
+ * * * Разобрать xml файл с данными
+ * * * Вернуть обработанные данные
+ * * * Удалить временную папку
+ * * Inc(dbf): файлы инклинометрии (База данных)
+ * * * Разбирает dbf файл и возвращает данные
+ * *
+ * Взаимодействие между изолятами
+ * Данные отправляемые вспомогательным изолятам:
+ *
+ * -e
+ * просьба закончить работу
+ *
+ * {{path}}
+ * просьба разобрать файл (увеличивает таск)
+ *
+ * Данные отправляемые главному изоляту:
+ * [id, ...]
+ *
+ * -e
+ * вспомогательный изолят закончил свою работу, ответ на просьбу закончить работу
+ *
+ * */
+
+/// Данные вспомогательных изолятов
+class IsoSub {}
+
 // Количество потоков
 const isoCount = 3;
 
 class IsoData {
-  int id; // Номер изолята (начиная с 1)
-  SendPort sendPortIn; // Порт через который изолят может отправлять сообщения
-  SendPort sendPortOut; // Порт через который главный изолят посылает сообщения
-  int jobs; // Количество задач в очереди, отслеживает главный изолят
-  Map<String, List<String>> map; // Таблица кодировок
+  final int id; // Номер изолята (начиная с 1)
+  final SendPort sendPort; // Порт для передачи данных главному изоляту
+  final Map<String, List<String>> charMaps; // Таблицы кодировок
+
+  IsoData(final int id, final SendPort sendPort, final charMaps)
+      : id = id,
+        sendPort = sendPort,
+        charMaps = Map.unmodifiable(charMaps);
 }
 
-void runIsolate(IsoData data) {
-  // Обработка файла
-  void workFile(final File file) {
-    file.readAsBytes().then((bytes) {
-      final cp = getMappingMax(getMappingRaitings(data.map, bytes));
-      final buffer = String.fromCharCodes(bytes
-          .map((i) => i >= 0x80 ? data.map[cp][i - 0x80].codeUnitAt(0) : i));
-      final lines = LineSplitter.split(buffer);
-      var section = '';
-      for (final line in lines) {
-        final clearLine = line.trim();
-        // Строку с комментарием пропускаем
-        if (clearLine.startsWith('#')) {
-          continue;
-        }
-        // Заголовок секции
-        if (clearLine.startsWith('~')) {
-          section = clearLine[1];
-          continue;
-        }
-        switch (section) {
-          case 'W':
-            if (line.isNotEmpty) {
-              final i0 = line.indexOf('.');
-              if (i0 < 0) {
-                assert(true);
-              }
-              final i1 = line.indexOf(' ', i0);
-              if (i1 < 0) {
-                assert(true);
-              }
-              final i2 = line.lastIndexOf(':');
-              if (i2 < 0) {
-                assert(true);
-              }
-            }
-            break;
-        }
-      }
-
-      data.sendPortIn.send('--job:${data.id}');
-    });
-  }
-
+void runIsolate(final IsoData data) {
   final receivePort = ReceivePort(); // Порт прослушиваемый изолятом
-  StreamSubscription<dynamic> subscriber; // Подписчик изолята
-  subscriber = receivePort.listen((final msg) {
+
+  receivePort.listen((final msg) {
     // Прослушивание сообщений полученных от главного изолята
     if (msg is String) {
-      if (msg == '--end') {
-        print('[${data.id}]: end');
-        subscriber.cancel();
-        data.sendPortIn.send('--end:${data.id}');
+      // Сообщение о закрытии
+      if (msg == '-e') {
+        receivePort.close();
+        print('sub[${data.id}]: end');
+        data.sendPort.send([data.id, '-e']);
       }
-    } else if (msg is File) {
-      workFile(msg);
     }
   });
 
   // Отправка данных для порта входящих сообщений
-  data.sendPortOut = receivePort.sendPort;
-  data.jobs = 0;
-  print('[${data.id}]: sync send data');
-  data.sendPortIn.send(data);
+  print('sub[${data.id}]: sync main');
+  data.sendPort.send([data.id, receivePort.sendPort]);
 }
 
 Future<void> main(List<String> args) async {
-  final map = Map.unmodifiable(await loadMappings('mappings'));
-  final iso = List<IsoData>.filled(isoCount, IsoData(), growable: false);
+  // Таблицы кодировок
+  final charMaps = Map.unmodifiable(await loadMappings('mappings'));
+  // Количество потоков
+  const isoCount = 3;
+  // Порты для передачи данных вспомогательным изолятам
+  final isoSends = List<SendPort>(isoCount);
+  // Количество задач у каждого вспомогательного изолята в очереди
+  final isoTasks = List<int>.filled(isoCount, 0);
   final isolate = List<Future<Isolate>>(isoCount);
-
-  final receivePort = ReceivePort(); // Порт прослушиваемый главным изолятом
-  StreamSubscription<dynamic> subscriber;
+  // Порт прослушиваемый главным изолятом
+  final receivePort = ReceivePort();
 
   Directory('.ag-47').createSync(recursive: true);
+  // == Функции начало =========================================================
+  bool isoTasksZero() {
+    for (final i in isoTasks) {
+      if (i != 0) {
+        return false;
+      }
+    }
+    return true;
+  }
 
-  void endAll() {
-    for (var i in iso) {
-      print('main: send [${i.id}] end');
-      i.sendPortOut.send('--end');
+  void isoSendToAll(final msg) {
+    for (var i = 0; i < isoCount; i++) {
+      print('main: send to sub[${i + 1}] "$msg"');
+      isoTasks[i] += 1;
+      isoSends[i].send(msg);
     }
   }
+  // == Функции конец ==========================================================
 
-  void thisEnd() {
-    print('main: end');
-    subscriber.cancel();
-  }
+  receivePort.listen((final data) {
+    if (data is List) {
+      // Передача порта для связи
+      if (data[1] is SendPort) {
+        isoTasks[data[0] - 1] = 0;
+        isoSends[data[0] - 1] = data[1];
+        print('main: sub[${data[0]}] synced');
+        if (isoTasksZero()) {
+          print('main: all subs synced');
 
-  void sendFile(final File file) {
-    final path = file.path.toLowerCase();
-    if (path.endsWith('.las')) {
-      var j = 0;
-      for (var i = 1; i < isoCount; i++) {
-        if (iso[i].jobs < iso[j].jobs) {
-          j = i;
+          isoSendToAll('-e');
         }
-      }
-      iso[j].jobs += 1;
-      iso[j].sendPortOut.send(file);
-    }
-  }
-
-  subscriber = receivePort.listen((data) {
-    // Прослушивание посланий от изолятов
-    if (data is IsoData) {
-      // Передача данных для синхронизации
-      print('main: [${data.id}] sync updated');
-      iso[data.id - 1] = data;
-      var b = true;
-      for (final i in iso) {
-        if (i.jobs != 0) {
-          b = false;
-        }
-      }
-      if (b) {
-        // jobs = 0 для каждого изолята, значит всё готово для работы
-        Directory('\\\\NAS\\Public\\common').list(recursive: true).listen((e) {
-          if (e is File) {
-            sendFile(e);
-          }
-        }, onDone: () {
-          endAll();
-        });
-      }
-    }
-    if (data is String) {
-      // Уведомление изолята о закрытии
-      if (data.startsWith('--end:')) {
-        iso[int.parse(data.substring(6)) - 1].id = 0;
-        var b = true;
-        for (final i in iso) {
-          if (i.id != 0) {
-            b = false;
+      } else if (data[1] is String) {
+        // Ответ на просьбу закрыться
+        if (data[1] == '-e') {
+          print('main: sub[${data[0]}] ended');
+          isoTasks[data[0] - 1] = 0;
+          if (isoTasksZero()) {
+            print('main: all subs ended');
+            receivePort.close();
           }
         }
-        if (b) {
-          // Все изоляты уведомили о закрытии и теперь можно закрыться самому
-          thisEnd();
-        }
-      } else
-      // Уведомление изолята о завершении обработки файла
-      if (data.startsWith('--job:')) {
-        iso[int.parse(data.substring(6)) - 1].jobs--;
       }
     }
   });
 
   // Создание изолятов
   for (var i = 0; i < isoCount; i++) {
-    iso[i].map = Map.unmodifiable(map);
-    iso[i].id = i + 1;
-    iso[i].jobs = 1;
-    iso[i].sendPortIn = receivePort.sendPort;
-    print('main: spawn [${i + 1}]');
-    isolate[i] =
-        Isolate.spawn(runIsolate, iso[i], debugName: 'isolate[${i + 1}]');
+    final data = IsoData(i + 1, receivePort.sendPort, charMaps);
+    isoTasks[i] = -1;
+    print('main: spawn sub[${i + 1}]');
+    isolate[i] = Isolate.spawn(runIsolate, data, debugName: 'sub[${i + 1}]');
   }
   // Ожидаем создания
   await Future.wait(isolate);
+  print('main: all subs spawned');
 }
